67e90fa5337acea4d500731d9c345baa28ed9e62
Nasca723 <47943557+Nasca723@users.noreply.github.com>
2021-11-03 00:23:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,70 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
+def handle_message(event):         
+##    if  re.match(".*笑死.*",event.message.text):
+##          line_bot_api.reply_message(
+##              event.reply_token,
+##              TextSendMessage(text='笑死'))
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    if  re.match(".*好想喝酒.*",event.message.text):   ##########  好想喝酒
+        
+        if event.source.user_id == alice:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='只與你又來了'))
+              
+        if event.source.user_id == sandy:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='小心計時器'))
+              
+        if event.source.user_id == han:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='亂講  你又不是只與'))
+
+    if  re.match("宏匯廣場",event.message.text):   ##########  宏匯廣場
+        
+        if event.source.user_id == alice:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='這是一個悲慘的故事'))
+              
+        if event.source.user_id == sandy:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='你還敢提?'))
+              
+        if event.source.user_id == han:
+              line_bot_api.reply_message(
+                  event.reply_token,
+                  TextSendMessage(text='弄得全身濕 辛苦你了'))
+
+    if  re.match(".*幹.*",event.message.text):  ######    幹
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='噓 不要亂罵髒話'))
+        
+    if  re.match("諧咖",event.message.text):  ######    諧咖
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='誰'))
+
+    if  re.match(".*哇咧.*",event.message.text):  ######    哇咧
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='在哇咧我打破你的頭'))
+
+    if  re.match(".*=.*=.*",event.message.text):  ######    哇咧
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='=='))
+        
+
+
+
+        
+##    line_bot_api.reply_message(
+##        event.reply_token,
+##        TextSendMessage(text=event.message.text))
